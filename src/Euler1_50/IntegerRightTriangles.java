d56965c1d97eff74b2cef87b0f58ad7d27ac0dec
Chjun-chi Chiu <chjunchi.chiu@gmail.com>
2019-02-23 14:32:59 +0100

    Euler problem 39

--- src/Euler1_50/IntegerRightTriangles.java
+++ src/Euler1_50/IntegerRightTriangles.java
@@ -1,5 +1,7 @@
 package Euler1_50;
 
+import java.util.HashMap;
+
 /**
  * 
  * Integer right triangles Problem 39 If p is the perimeter of a right angle
@@ -10,13 +12,41 @@ package Euler1_50;
  * 
  * For which value of p ≤ 1000, is the number of solutions maximised?
  * 
- * @author Chjun-chi 
+ * @author Chjun-chi
  *
  */
 public class IntegerRightTriangles {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int a =1; a < 1000; a++) {
+			for (int b = 1; b < 1000; b++) {
+				for (int c = 1; c < 1000; c++) {
+					int key = a + b + c;
+					if ((key<=1000)&& (a * a + b * b) == (c * c)) {
+						if (map.containsKey(key)) {
+							int value = map.get(key);
+							map.put(key, ++value);
+						} else {
+							map.put(key, 1);
+						}
+					}
+
+				}
+			}
+		}
+		int max = 0;
+		int p = 0;
+		for (int key : map.keySet()) {
+			int value = map.get(key);
+			if (value > max) {
+				max = value;
+				p = key;
+			}
+
+		}
+		System.out.println("Maximum p: " + p);
+
 	}
 
 }
