5318f485233c4b9c709c6553d3cbf315eee21672
xarczuz <chjunchi.chiu@gmail.com>
2019-04-01 21:08:21 +0200

    euler p63

---
+++ src/Euler50to100/OddPeriodSquareRoots.java
@@ -0,0 +1,21 @@
+package Euler50to100;
+
+
+/**
+ * Problem 64
+
+ * @author Chjun-chi
+ *
+ */
+public class OddPeriodSquareRoots {
+
+	public static void main(String[] args) {
+
+		System.out.println(Math.sqrt(23));
+		System.out.println((double)((double)1/7));
+		System.out.println(Math.sqrt(2));
+		System.out.println(Math.sqrt(1));
+	
+	}
+
+}
