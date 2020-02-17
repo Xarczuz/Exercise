7ecea85abd9f45637162fedf7d8072d9c5c4892a
Xarczuz <chjunchi.chiu@gmail.com>
2020-02-17 10:28:59 +0100

    leetcode 1323

---
+++ src/LeetCode/Maximum69Number1323.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+public class Maximum69Number1323 {
+    public static int maximum69Number(int num) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(num);
+        for (int i = 0; i < stringBuilder.length(); i++) {
+            if (stringBuilder.substring(i, i + 1).equals("6")) {
+                stringBuilder = stringBuilder.replace(i, i + 1, "9");
+                return Integer.parseInt(stringBuilder.toString());
+            }
+        }
+        return num;
+    }
+
+    public static int maximum69Number2(int num) {
+        String nr = String.valueOf(num);
+        char[] chars = nr.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == 54) {
+                chars[i] = 57;
+                return Integer.parseInt(String.valueOf(chars));
+            }
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maximum69Number2(9696));
+    }
+}
