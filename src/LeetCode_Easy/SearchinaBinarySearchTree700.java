892c14c4a71985536b02b9a74b303b5067def58d
Xarczuz <chjunchi.chiu@gmail.com>
2020-02-26 16:02:59 +0100

    leetcode 700

---
+++ src/LeetCode_Easy/SearchinaBinarySearchTree700.java
@@ -0,0 +1,34 @@
+package LeetCode_Easy;
+
+import java.util.Stack;
+
+public class SearchinaBinarySearchTree700 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static TreeNode searchBST(TreeNode root, int val) {
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            if (root.val == val)
+                return root;
+            if (root.left != null)
+                stack.push(root.left);
+            if (root.right != null)
+                stack.push(root.right);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
