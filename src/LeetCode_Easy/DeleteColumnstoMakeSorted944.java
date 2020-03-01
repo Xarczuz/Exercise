74fcb0fc34ce072e75450bf454755c2329d03b17
xarczuz <chjunchi.chiu@gmail.com>
2020-03-01 16:33:38 +0100

    leetcode 944

---
+++ src/LeetCode_Easy/DeleteColumnstoMakeSorted944.java
@@ -0,0 +1,16 @@
+package LeetCode_Easy;
+
+public class DeleteColumnstoMakeSorted944 {
+    public int minDeletionSize(String[] A) {
+        int count = 0;
+        for (int i = 0; i < A[0].length(); i++) {
+            for (int j = 1; j < A.length; j++) {
+                if (A[j - 1].charAt(i) > A[j].charAt(i)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
