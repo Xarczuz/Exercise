3804843c19ef5c53e75610202fe8fbdf2366e960
xarczuz <chjunchi.chiu@gmail.com>
2020-03-01 14:58:48 +0100

    leetcode 590

--- src/LeetCode_Easy/N_aryTreePostorderTraversal590.java
+++ src/LeetCode_Easy/N_aryTreePostorderTraversal590.java
@@ -1,20 +1,26 @@
 package LeetCode_Easy;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
 public class N_aryTreePostorderTraversal590 {
 
     public List<Integer> postorder(Node root) {
+        if (root == null) {
+            return new LinkedList<>();
+        }
+        LinkedList<Integer> rArr = new LinkedList<>();
         Stack<Node> stack = new Stack<>();
         stack.push(root);
         while (!stack.isEmpty()) {
             root = stack.pop();
+            rArr.addFirst(root.val);
             for (Node n : root.children) {
-
+                stack.push(n);
             }
         }
-        return null;
+        return rArr;
     }
 
     public static void main(String[] args) {
