b2a07b6eddf348ef66c613acd271c92ff3970cf8
xarczuz <chjunchi.chiu@gmail.com>
2020-03-01 16:06:31 +0100

    leetcode 1365

---
+++ src/LeetCode_Easy/HowManyNumbersAreSmallerThantheCurrentNumber1365.java
@@ -0,0 +1,62 @@
+package LeetCode_Easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class HowManyNumbersAreSmallerThantheCurrentNumber1365 {
+
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] arr = new int[nums.length];
+        int i = 0;
+        for (int n : nums) {
+            int count = 0;
+            for (int k : nums) {
+                if (n > k) {
+                    count++;
+                }
+            }
+            arr[i] = count;
+            i++;
+        }
+        return arr;
+    }
+
+    public int[] smallerNumbersThanCurrent2(int[] nums) {
+        int[] arr = new int[nums.length];
+        HashMap<Integer, Integer> memory = new HashMap<>();
+        int i = 0;
+        for (int n : nums) {
+            int count = 0;
+            if (memory.containsKey(n)) {
+                count = memory.get(n);
+            } else {
+                for (int k : nums) {
+                    if (n > k) {
+                        count++;
+                    }
+                }
+                memory.put(n, count);
+            }
+            arr[i] = count;
+            i++;
+        }
+        return arr;
+    }
+
+    public int[] smallerNumbersThanCurrent3(int[] nums) {
+        int b[] = nums.clone();
+        Arrays.sort(nums);
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                continue;
+            }
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < b.length; i++) {
+            nums[i] = map.get(b[i]);
+        }
+        return nums;
+    }
+
+}
