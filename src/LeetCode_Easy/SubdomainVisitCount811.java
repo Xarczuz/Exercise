d91dbf726114b3657894da0ac33af49db8320b6b
xarczuz <chjunchi.chiu@gmail.com>
2020-03-03 00:26:41 +0100

    LeetCode 811

--- src/LeetCode_Easy/SubdomainVisitCount811.java
+++ src/LeetCode_Easy/SubdomainVisitCount811.java
@@ -5,35 +5,50 @@ import java.util.HashMap;
 import java.util.List;
 
 public class SubdomainVisitCount811 {
-    static HashMap<String, Integer> visitedSites = new HashMap<>();
 
     public static List<String> subdomainVisits(String[] cpdomains) {
-
+        HashMap<String, Integer> visitedSites = new HashMap<>();
         for (String s : cpdomains) {
-            splitter(s);
+            splitter(s, visitedSites);
         }
-        List<String> arr = new ArrayList<>();
+        List<String> arr = new ArrayList<>(visitedSites.size());
         visitedSites.forEach((s, integer) -> {
-            StringBuilder sb = new StringBuilder();
-            sb.append(integer).append(" ").append(s);
-            arr.add(sb.toString());
+            arr.add(new StringBuilder().append(integer).append(" ").append(s).toString());
         });
         return arr;
     }
 
-    public static void splitter(String s) {
+    public static void splitter(String s, HashMap<String, Integer> visitedSites) {
         int visited = 0;
-        for (int i = 0; i < s.length(); i++) {
+        int dots = 0;
+        int i = 0;
+        for (; i < s.length(); i++) {
             if (s.charAt(i) == ' ') {
-                visited = Integer.parseInt(s.substring(0, i));
                 String link = s.substring(i + 1);
                 visitedSites.put(link, visitedSites.getOrDefault(link, 0) + visited);
+                break;
             }
+            visited = visited * 10 + s.charAt(i) - '0';
+        }
+        for (; i < s.length(); i++) {
             if (s.charAt(i) == '.') {
                 String link = s.substring(i + 1);
                 visitedSites.put(link, visitedSites.getOrDefault(link, 0) + visited);
+                dots++;
+                if (dots == 2) {
+                    break;
+                }
             }
         }
+    }
+
+    private static int parseInt(String s, int visited) {
+        int g = 0;
+        while (g < s.length() && s.charAt(g) != ' ') {
+            visited = visited * 10 + s.charAt(g) - '0';
+            g++;
+        }
+        return visited;
     }
 
     public static void main(String[] args) {
