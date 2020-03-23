2a1049754b7080ccc4a1b6916af84c27d3e8968c
xarczuz <chjunchi.chiu@gmail.com>
2020-03-23 12:53:45 +0100

    LeetCode 557

--- src/LeetCode_Easy/LeetCode_557_ReverseWordsinaStringIII.java
+++ src/LeetCode_Easy/LeetCode_557_ReverseWordsinaStringIII.java
@@ -2,6 +2,33 @@ package LeetCode_Easy;
 
 public class LeetCode_557_ReverseWordsinaStringIII {
 
+    public static void main(String[] args) {
+        System.out.println(new LeetCode_557_ReverseWordsinaStringIII().reverseWords3("leet code code"));
+    }
+
+    public String reverseWords3(String s) {
+        char[] c = s.toCharArray();
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] == ' ') {
+                end = i - 1;
+                reverseString(c, start, end);
+                start = i + 1;
+            }
+        }
+        reverseString(c, start, c.length - 1);
+        return new String(c);
+    }
+
+    private void reverseString(char[] s, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            char swap = s[i];
+            s[i] = s[j];
+            s[j] = swap;
+        }
+    }
+
     public String reverseWords2(String s) {
         StringBuilder sb = new StringBuilder();
         StringBuilder sbRes = new StringBuilder();
