85f467f67026a5c85c466131b8758ce923d1f812
xarczuz <chjunchi.chiu@gmail.com>
2020-04-23 20:13:03 +0200

    LeetCode 202

---
+++ src/LeetCode_Easy/LeetCode_202_HappyNumber.java
@@ -0,0 +1,32 @@
+package LeetCode_Easy;
+
+import java.util.HashSet;
+
+public class LeetCode_202_HappyNumber {
+    public static void main(String[] args) {
+        System.out.println(new LeetCode_202_HappyNumber().isHappy(19));
+    }
+
+    public boolean isHappy(int n) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        while (n > 0) {
+            int t = 0;
+            int f = n;
+            while (f > 0) {
+                int r = f % 10;
+                t += r * r;
+                f /= 10;
+            }
+            if (n == 1) {
+                return true;
+            }
+            if (hashSet.contains(t)) {
+                return false;
+            }
+            n = t;
+            hashSet.add(t);
+        }
+        return false;
+    }
+
+}
