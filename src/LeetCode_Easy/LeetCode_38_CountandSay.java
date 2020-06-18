cd837d2686416837382948f19bc50a934e503c23
Xarczuz <chjunchi.chiu@gmail.com>
2020-06-18 18:01:28 +0200

    leetcode 38

--- src/LeetCode_Easy/LeetCode_38_CountandSay.java
+++ src/LeetCode_Easy/LeetCode_38_CountandSay.java
@@ -1,19 +1,42 @@
 package LeetCode_Easy;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class LeetCode_38_CountandSay {
+    HashMap<Integer, String> hashMap = new HashMap<>();
+    boolean once = true;
 
     public String countAndSay(int n) {
+        if (once) {
+            List<Integer> list = new ArrayList<>();
+            list.add(1);
+            hashMap.put(1, "1");
+            int i = 2;
+            for (; i <= 30; i++) {
+                list = countNext(list);
+                StringBuilder sb = new StringBuilder();
+                list.forEach(sb::append);
+                hashMap.put(i, sb.toString());
+            }
+            once = false;
+        }
+        return hashMap.get(n);
+    }
+
+    public String countAndSay2(int n) {
+
         List<Integer> list = new ArrayList<>();
         list.add(1);
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < 30; i++) {
             list = countNext(list);
         }
+
         StringBuilder sb = new StringBuilder();
-        list.forEach(integer -> sb.append(integer));
+        list.forEach(sb::append);
+
         return sb.toString();
     }
 
@@ -46,6 +69,8 @@ public class LeetCode_38_CountandSay {
     }
 
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        System.out.println("Start");
         System.out.println(new LeetCode_38_CountandSay().countAndSay(1));
         System.out.println(new LeetCode_38_CountandSay().countAndSay(2));
         System.out.println(new LeetCode_38_CountandSay().countAndSay(3));
@@ -56,5 +81,22 @@ public class LeetCode_38_CountandSay {
         System.out.println(new LeetCode_38_CountandSay().countAndSay(8));
         System.out.println(new LeetCode_38_CountandSay().countAndSay(9));
         System.out.println(new LeetCode_38_CountandSay().countAndSay(10));
+        long end = System.currentTimeMillis() - start;
+        System.out.println("End: " + end);
+        start = System.currentTimeMillis();
+        System.out.println("Start2");
+        LeetCode_38_CountandSay l38 = new LeetCode_38_CountandSay();
+        System.out.println(l38.countAndSay(1));
+        System.out.println(l38.countAndSay(2));
+        System.out.println(l38.countAndSay(3));
+        System.out.println(l38.countAndSay(4));
+        System.out.println(l38.countAndSay(5));
+        System.out.println(l38.countAndSay(6));
+        System.out.println(l38.countAndSay(7));
+        System.out.println(l38.countAndSay(8));
+        System.out.println(l38.countAndSay(9));
+        System.out.println(l38.countAndSay(10));
+        end = System.currentTimeMillis() - start;
+        System.out.println("End2: " + end);
     }
 }
