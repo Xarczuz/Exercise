bfc51be6b44484c30052b3f2c04d804848ca03d7
xarczuz <chjunchi.chiu@gmail.com>
2020-09-16 16:29:34 +0200

    LeetCode 1189

--- src/LeetCode_Easy/LeetCode_1189_MaximumNumberofBalloons.java
+++ src/LeetCode_Easy/LeetCode_1189_MaximumNumberofBalloons.java
@@ -11,8 +11,8 @@ public class LeetCode_1189_MaximumNumberofBalloons {
         min = Math.min(chars[0], min);
         min = Math.min(chars[1], min);
         min = Math.min(chars[11] / 2, min);
-        min = Math.min(chars[13], min);
-        min = Math.min(chars[14] / 2, min);
+        min = Math.min(chars[13] / 2, min);
+        min = Math.min(chars[14], min);
         return min;
     }
 }
