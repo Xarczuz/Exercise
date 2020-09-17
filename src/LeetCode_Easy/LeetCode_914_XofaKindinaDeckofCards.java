20ad2f611169cffea40727d4e1c106c934871ff5
xarczuz <chjunchi.chiu@gmail.com>
2020-09-17 14:10:44 +0200

    LeetCode 914

--- src/LeetCode_Easy/LeetCode_914_XofaKindinaDeckofCards.java
+++ src/LeetCode_Easy/LeetCode_914_XofaKindinaDeckofCards.java
@@ -9,10 +9,11 @@ public class LeetCode_914_XofaKindinaDeckofCards {
         for (int value : deck) {
             hashMap.put(value, hashMap.getOrDefault(value, 0) + 1);
         }
-        for (int i = 2; i <= 9; i++) {
+
+        for (int x = 2; x <= 9; x++) {
             int checkedValues = 0;
             for (int value : hashMap.values()) {
-                if (value % i != 0) {
+                if (value % x != 0) {
                     break;
                 }
                 checkedValues++;
@@ -24,9 +25,20 @@ public class LeetCode_914_XofaKindinaDeckofCards {
         return false;
     }
 
-    public static void main(String[] args) {
+   static public int gcd(int divisor, int value) {
+        if (value == 0) {
+            return divisor;
+        } else {
+            return gcd(value, divisor % value);
+        }
+    }
 
-        hasGroupsSizeX(new int[]{1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2});
-        hasGroupsSizeX(new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2});
+    public static void main(String[] args) {
+        System.out.println(gcd(0,14));
+        System.out.println(gcd(14,28));
+
+
+       // hasGroupsSizeX(new int[]{1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2});
+      //  hasGroupsSizeX(new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2});
     }
 }
