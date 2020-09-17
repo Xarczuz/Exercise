ee188a79fd2fafbb9c311012aad103f0e51d0b84
xarczuz <chjunchi.chiu@gmail.com>
2020-09-17 14:57:30 +0200

    LeetCode 496

--- src/LeetCode_Easy/LeetCode_496_NextGreaterElementI.java
+++ src/LeetCode_Easy/LeetCode_496_NextGreaterElementI.java
@@ -1,17 +1,21 @@
 package LeetCode_Easy;
 
+import java.util.HashMap;
+
 public class LeetCode_496_NextGreaterElementI {
 
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] res = new int[nums1.length];
-
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int j = 0; j < nums2.length; j++) {
+            hashMap.put(nums2[j], j + 1);
+        }
         for (int i = 0; i < nums1.length; i++) {
             boolean notFound = true;
-            int next = 0;
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums2[j] == nums1[i]) {
-                    next = j;
-                }
+            int next = hashMap.getOrDefault(nums1[i], -1);
+            if (next == -1) {
+                res[i] = -1;
+                continue;
             }
             for (int j = next; j < nums2.length; j++) {
                 if (nums2[j] > nums1[i]) {
