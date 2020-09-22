f6a9d0a43e65394e4876400937a62af5370b73d5
xarczuz <chjunchi.chiu@gmail.com>
2020-09-22 23:47:16 +0200

    LeetCode 989

--- src/LeetCode_Easy/LeetCode_989_AddtoArrayFormofInteger.java
+++ src/LeetCode_Easy/LeetCode_989_AddtoArrayFormofInteger.java
@@ -6,31 +6,24 @@ import java.util.List;
 public class LeetCode_989_AddtoArrayFormofInteger {
 
     public static List<Integer> addToArrayForm(int[] A, int K) {
-
         int indexA = A.length - 1;
-        String B = String.valueOf(K);
-        int indexB = B.length() - 1;
         LinkedList<Integer> integers = new LinkedList<>();
         int carry = 0;
-        while (indexA >= 0 || indexB >= 0) {
+        while (indexA >= 0 || K != 0) {
             int n1 = 0;
             int n2 = 0;
             if (indexA >= 0) {
                 n1 = A[indexA];
                 indexA--;
             }
-            if (indexB >= 0) {
+            if (K > 0) {
                 n2 = K % 10;
                 K /= 10;
-                indexB--;
             }
-
             int sum = carry + n1 + n2;
-            int rest = sum % 10;
             carry = sum / 10;
-            integers.addFirst(rest);
+            integers.addFirst(sum % 10);
         }
-
         if (carry > 0) {
             integers.addFirst(carry);
         }
