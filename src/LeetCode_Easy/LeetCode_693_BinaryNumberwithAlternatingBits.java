aa01c36cbbf936425f1bbaf48eece8043ff6b540
xarczuz <chjunchi.chiu@gmail.com>
2020-09-24 23:51:13 +0200

    LeetCode 693

--- src/LeetCode_Easy/LeetCode_693_BinaryNumberwithAlternatingBits.java
+++ src/LeetCode_Easy/LeetCode_693_BinaryNumberwithAlternatingBits.java
@@ -17,8 +17,22 @@ public class LeetCode_693_BinaryNumberwithAlternatingBits {
         return true;
     }
 
-    public static void main(String[] args) {
-        System.out.println(hasAlternatingBits(4));
+    public static boolean hasAlternatingBits2(int n) {
+        String binary = Integer.toBinaryString(n);
+        System.out.println(binary);
+        int d = n & 1;
+        for (int i = 1; i <= binary.length(); i++) {
+            int c = (n >> i) & 1;
+            System.out.println(c + " " + d);
+            if (d == c) {
+                return false;
+            }
+            d = c;
+        }
+        return true;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(hasAlternatingBits2(5));
     }
 }
