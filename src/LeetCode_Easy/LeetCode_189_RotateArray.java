236e19e1eee9d0f580bf60d9d11ce33bdcc4b8ec
xarczuz <chjunchi.chiu@gmail.com>
2020-09-25 15:17:34 +0200

    LeetCode 189

---
+++ src/LeetCode_Easy/LeetCode_189_RotateArray.java
@@ -0,0 +1,36 @@
+package LeetCode_Easy;
+
+import java.util.Arrays;
+
+public class LeetCode_189_RotateArray {
+
+    public static void rotate(int[] nums, int k) {
+        int moveXTimes = k % nums.length;
+        int[] copy = nums.clone();
+        for (int i = 0; i < nums.length; i++) {
+            int pos = (i + moveXTimes) % nums.length;
+            nums[pos] = copy[i];
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void main(String[] args) {
+        rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
+        rotate(new int[]{1, 2, 3, 4, 5, 6}, 2); // 3,99,-1,-100
+
+    }
+}
+//[1,2,3,4,5,6,7]
+//[5,6,7,1,2,3,4]
+//
+//
+
+
+
+
+
+
+
+
+
+
