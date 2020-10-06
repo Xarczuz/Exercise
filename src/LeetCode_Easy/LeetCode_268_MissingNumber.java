7109273564180a5f6c534da34ee0534bbf0471ee
xarczuz <chjunchi.chiu@gmail.com>
2020-10-06 16:35:45 +0200

    LeetCode 268

--- src/LeetCode_Easy/LeetCode_268_MissingNumber.java
+++ src/LeetCode_Easy/LeetCode_268_MissingNumber.java
@@ -3,14 +3,21 @@ package LeetCode_Easy;
 public class LeetCode_268_MissingNumber {
 
     public int missingNumber(int[] nums) {
-        int sum1 = 0;
+        int sum1 = (nums.length * (nums.length + 1)) / 2;
         int sum2 = 0;
-        for (int i = 0; i <= nums.length; i++) {
-            sum1 += i;
-        }
         for (int i = 0; i < nums.length; i++) {
             sum2 += nums[i];
         }
         return Math.abs(sum1 - sum2);
+    }
+
+    public int missingNumber2(int[] nums) {
+
+        int xor = 0, i = 0;
+        for (i = 0; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+
+        return xor ^ i;
     }
 }
