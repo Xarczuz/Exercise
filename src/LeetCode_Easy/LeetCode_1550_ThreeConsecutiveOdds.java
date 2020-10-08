69ed9a6b3bf8ce4cc4f6cb1caf351dc3f697d1d8
xarczuz <chjunchi.chiu@gmail.com>
2020-10-08 13:25:09 +0200

    LeetCode 1550

--- src/LeetCode_Easy/LeetCode_1550_ThreeConsecutiveOdds.java
+++ src/LeetCode_Easy/LeetCode_1550_ThreeConsecutiveOdds.java
@@ -7,11 +7,11 @@ public class LeetCode_1550_ThreeConsecutiveOdds {
         for (int i : arr) {
             if (i % 2 != 0) {
                 counter++;
+                if (counter == 3) {
+                    return true;
+                }
             } else {
                 counter = 0;
-            }
-            if (counter == 3) {
-                return true;
             }
         }
         return false;
