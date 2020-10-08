f7217ce7989238b196c472fe8775329827634de1
xarczuz <chjunchi.chiu@gmail.com>
2020-10-08 11:16:45 +0200

    LeetCode 278

--- src/LeetCode_Easy/LeetCode_278_FirstBadVersion.java
+++ src/LeetCode_Easy/LeetCode_278_FirstBadVersion.java
@@ -1,9 +1,7 @@
 package LeetCode_Easy;
 
-import java.util.stream.IntStream;
-
 public class LeetCode_278_FirstBadVersion {
-    static int badVersion = 4;
+    static int badVersion = 1702766719;
 
     public static boolean isBadVersion(int version) {
         if (version >= badVersion) {
@@ -16,19 +14,44 @@ public class LeetCode_278_FirstBadVersion {
 
     public static int firstBadVersion(int n) {
         minBad = n;
-        int[] arr = IntStream.range(1, n).toArray();
-
         if (isBadVersion(n / 2)) {
-            //  findMin(Arrays.copyOfRange(),1,n/2);
+            minBad = Math.min(minBad, n / 2);
+            findMin(1, (n / 2));
         } else {
-            // findMin(Arrays.copyOfRange(),1,n/2);
+            findMin((n / 2) + 1, n);
+        }
+        return minBad;
+    }
+
+    private static void findMin(int i, int i1) {
+        if (i == i1) {
+            minBad = Math.min(minBad, i1);
+            return;
+        }
+        int mid = (int) (((long) i + (long) i1) / 2);
+        if (isBadVersion(mid)) {
+            findMin(1, mid);
+        } else {
+            findMin((mid + 1), i1);
+        }
+    }
+
+    public int firstBadVersion2(int n) {
+        int low = 1, high = n;
+        while (low <= high) {
+            int middle = (int) (((long) low + (long) high) / 2);
+            if (isBadVersion(middle)) {
+                high = middle - 1;
+            } else {
+                low = middle + 1;
+            }
 
         }
+        return low;
 
-        return n;
     }
 
     public static void main(String[] args) {
-        System.out.println(5 / 2);
+        System.out.println(firstBadVersion(2126753390));
     }
 }
