91bda3fe2688e2e5b76acaf5e0696051c7be01c0
xarczuz <chjunchi.chiu@gmail.com>
2020-10-14 12:09:50 +0200

    LeetCode 643

---
+++ src/LeetCode_Easy/LeetCode_367_ValidPerfectSquare.java
@@ -0,0 +1,12 @@
+package LeetCode_Easy;
+
+public class LeetCode_367_ValidPerfectSquare {
+
+    public boolean isPerfectSquare(int num) {
+        if (num == 1) {
+            return true;
+        }
+        double d = Math.sqrt(num);
+        return d - Math.floor(d) == 0;
+    }
+}
