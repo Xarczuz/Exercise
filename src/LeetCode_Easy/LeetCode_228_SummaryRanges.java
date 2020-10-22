9819f11cd4035822a67d18b1bcb37155791b6042
xarczuz <!!11!!11""22""22HunterXcc>
2020-10-22 15:27:18 +0200

    LeetCode_228_SummaryRanges

--- src/LeetCode_Easy/LeetCode_228_SummaryRanges.java
+++ src/LeetCode_Easy/LeetCode_228_SummaryRanges.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class LeetCode_228_SummaryRanges {
 
-
     public static List<String> summaryRanges(int[] nums) {
         ArrayList<String> arr = new ArrayList<>();
         if (nums.length == 0) {
@@ -16,39 +15,38 @@ public class LeetCode_228_SummaryRanges {
             arr.add(String.valueOf(prev));
             return arr;
         }
-        int startIndex = 0;
+        int startInterval = 0;
         for (int i = 1; i < nums.length; i++) {
-
             if (prev + 1 == nums[i]) {
                 if (i == nums.length - 1) {
-                    StringBuilder st = new StringBuilder();
-                    st.append(nums[startIndex]).append("->").append(nums[i]);
-                    arr.add(st.toString());
+                    arr.add(getString(nums, nums[startInterval], i));
                 }
-                prev = nums[i];
-            } else if (prev + 1 != nums[i]) {
-                if (startIndex == i - 1) {
+            } else {
+                if (startInterval == i - 1) {
                     arr.add(String.valueOf(prev));
                 } else {
-                    StringBuilder st = new StringBuilder();
-                    st.append(nums[startIndex]).append("->").append(nums[i - 1]);
-                    arr.add(st.toString());
+                    arr.add(getString(nums, nums[startInterval], i - 1));
                 }
-                startIndex = i;
                 if (i == nums.length - 1) {
                     arr.add(String.valueOf(nums[i]));
                 }
-                prev = nums[i];
+                startInterval = i;
             }
+            prev = nums[i];
         }
         return arr;
     }
 
-    public static void main(String[] args) {
-//        System.out.println(summaryRanges(new int[] {0}));
-//        System.out.println(summaryRanges(new int[] {}));
+    private static String getString(int[] nums, int num, int i2) {
+        StringBuilder st = new StringBuilder();
+        st.append(num).append("->").append(nums[i2]);
+        return st.toString();
+    }
 
-//        System.out.println(summaryRanges(new int[]{0, 1, 2, 3, 4, 5}));
+    public static void main(String[] args) {
+        System.out.println(summaryRanges(new int[]{0}));
+        System.out.println(summaryRanges(new int[]{}));
+        System.out.println(summaryRanges(new int[]{0, 1, 2, 3, 4, 5}));
         System.out.println(summaryRanges(new int[]{-10, -9, -3}));
         System.out.println(summaryRanges(new int[]{0, 1, 2, 4, 5, 7}));
         System.out.println(summaryRanges(new int[]{1, 3}));
