1b89b31c1a00866d6926695c3f362192ffc464f5
xarczuz <!!11!!11""22""22HunterXcc>
2020-10-26 19:26:20 +0100

    LeetCode_819_MostCommonWord

---
+++ src/LeetCode_Easy/LeetCode_819_MostCommonWord.java
@@ -0,0 +1,37 @@
+package LeetCode_Easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+
+public class LeetCode_819_MostCommonWord {
+
+    public String mostCommonWord(String paragraph, String[] banned) {
+        HashSet<String> bannedSet = new HashSet<>(Arrays.asList(banned));
+        bannedSet.add("");
+        HashMap<String, Integer> wordMapper = new HashMap<>();
+        StringBuilder st = new StringBuilder();
+        for (char c : paragraph.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                st.append(c);
+            } else if (c >= 'A' && c <= 'Z') {
+                st.append((char) (c + 32));
+            } else {
+                wordMapper.put(st.toString(), wordMapper.getOrDefault(st.toString(), 0) + 1);
+                st = new StringBuilder();
+            }
+        }
+        wordMapper.put(st.toString(), wordMapper.getOrDefault(st.toString(), 0) + 1);
+        int max = 0;
+        String mostCommon = "";
+        for (Map.Entry<String, Integer> set : wordMapper.entrySet()) {
+            if (set.getValue() > max && !bannedSet.contains(set.getKey())) {
+                mostCommon = set.getKey();
+                max = set.getValue();
+            }
+        }
+        return mostCommon;
+    }
+}
