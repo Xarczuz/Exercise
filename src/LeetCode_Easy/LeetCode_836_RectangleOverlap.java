91859603e99e216e96ead1bb41750642032e3e53
xarczuz <!!11!!11""22""22HunterXcc>
2020-11-01 18:51:54 +0100

    LeetCode_836_RectangleOverlap

--- src/LeetCode_Easy/LeetCode_836_RectangleOverlap.java
+++ src/LeetCode_Easy/LeetCode_836_RectangleOverlap.java
@@ -3,6 +3,20 @@ package LeetCode_Easy;
 public class LeetCode_836_RectangleOverlap {
 
     public static boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        return rec1[0] < rec2[2] && rec2[0] < rec1[2] && rec1[1] < rec2[3] && rec2[1] < rec1[3];
+        int x1 = rec1[0];
+        int y1 = rec1[1];
+        int x2 = rec1[2];
+        int y2 = rec1[3];
+
+        int z1 = rec2[0];
+        int w1 = rec2[1];
+        int z2 = rec2[2];
+        int w2 = rec2[3];
+
+        if (x1 >= z2 || x2 <= z1 || y1 >= w2 || y2 <= w1) return false;
+
+        //take care of a line, not rectangular
+        if (x1 == x2 || y1 == y2 || z1 == z2 || w1 == w2) return false;
+        return true;
     }
 }
