fcfd8d837f2d543cf7f65be117ffa81e11b5d50e
xarczuz <!!11!!11""22""22HunterXcc>
2020-11-01 18:45:35 +0100

    LeetCode_836_RectangleOverlap

---
+++ src/LeetCode_Easy/LeetCode_836_RectangleOverlap.java
@@ -0,0 +1,8 @@
+package LeetCode_Easy;
+
+public class LeetCode_836_RectangleOverlap {
+
+    public static boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        return rec1[0] < rec2[2] && rec2[0] < rec1[2] && rec1[1] < rec2[3] && rec2[1] < rec1[3];
+    }
+}
